1f863d77360b79fcf3f6d40f2268148069b82c0b
overthestream <njhnjh02@naver.com>
2024-07-17 17:16:11 +0900

    format: biome

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NotAcceptableException, ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 
+import cors from "cors";
 import { AppModule } from "./app.module";
 import { HttpExceptionFilter } from "./httpException.filter";
 import { InternalErrorFilter } from "./internal-error.filter";
-import cors from "cors";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new InternalErrorFilter(), new HttpExceptionFilter());
@@ -12,7 +12,7 @@ async function bootstrap() {
     cors({
       origin: ["http://localhost:5173", "https://devkor.club"],
       // TODO: 배포 후 CORS 설정 변경
-    })
+    }),
   );
   app.useGlobalPipes(
     new ValidationPipe({
@@ -21,15 +21,15 @@ async function bootstrap() {
         const messages = errors
           .map((error) => {
             return `<${error.property}> ${Object.values(error.constraints).join(
-              " "
+              " ",
             )}`;
           })
           .join(" ");
         return new NotAcceptableException(
-          `입력값이 유효하지 않습니다 - ${messages}`
+          `입력값이 유효하지 않습니다 - ${messages}`,
         );
       },
-    })
+    }),
   );
   await app.listen(3071);
 }
